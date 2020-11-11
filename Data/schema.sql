5d92928a1983bc30d4d4e5a81469417cc249c2b2
Sooraj <51398824+ssooraj2000@users.noreply.github.com>
2020-11-11 14:19:59 +0530

    Updated Schema

--- Data/schema.sql
+++ Data/schema.sql
@@ -1,6 +1,6 @@
 drop table if exists `Twilio_Numbers`;
 create table `Twilio_Numbers`(
-    `Number` int(10) primary key);
+	`Number` varchar(15) primary key);
 
 drop table if exists `Session`;
 create table `Session`(
@@ -12,7 +12,7 @@ create table `Session`(
 drop table if exists `Used_By`;
 create table `Used_By`(
     `SID` int, 
-    `Number` int(10), 
+    `Number` varchar(15), 
     foreign key(`SID`) references Session(`SID`), 
     foreign key(`Number`) references Twilio_Numbers(`Number`));
 
